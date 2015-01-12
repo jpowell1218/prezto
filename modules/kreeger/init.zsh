200df09d5e0715389d74cddc717a5554067f6714
Ben Kreeger <benjaminkreeger@gmail.com>
2015-01-12 08:45:10 -0600

    Add my own module with a handle of my old functions/aliases.

---
+++ modules/kreeger/init.zsh
@@ -0,0 +1,14 @@
+# if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
+#   source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
+# fi
+
+# Load environment vars.
+source "${ZDOTDIR:-$HOME}/.zprezto/modules/kreeger/settings/captainu"
+
+# Load functions.
+source "${ZDOTDIR:-$HOME}/.zprezto/modules/kreeger/bundles/captainu"
+source "${ZDOTDIR:-$HOME}/.zprezto/modules/kreeger/bundles/development"
+source "${ZDOTDIR:-$HOME}/.zprezto/modules/kreeger/bundles/git"
+
+source "${ZDOTDIR:-$HOME}/.zprezto/modules/kreeger/aliases/git"
+source "${ZDOTDIR:-$HOME}/.zprezto/modules/kreeger/aliases/ruby"
